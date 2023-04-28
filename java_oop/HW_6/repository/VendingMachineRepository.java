066c882c2bc38e86aa64ec543511c426ccf4fb00
alex1331 <danilin999@bk.ru>
2023-04-28 09:39:34 +0300

    add lesson_6

---
+++ java_oop/HW_6/repository/VendingMachineRepository.java
@@ -0,0 +1,11 @@
+package java_oop.HW_6.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java_oop.HW_6.model.Product;
+
+public class VendingMachineRepository {
+    static public List<Product> productList = new ArrayList<>();
+
+}
